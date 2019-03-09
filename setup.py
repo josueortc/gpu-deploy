7ed398942f2071ba3f00b6f535f01b311c35830d
ecobost <emcobost@gmail.com>
2019-03-08 22:45:46 -0600

    Make gpu_deploy a single module

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
-from setuptools import setup, find_packages
+from setuptools import setup
 from os import path
-
-here = path.abspath(path.dirname(__file__))
 
 setup(
     name='gpu-deploy',
-    version='0.0',
-    description='GPU managment for nvidia-docker',
+    version='0.0.1',
+    description='GPU management for nvidia-docker',
     author='Eric Y. Wang',
     author_email='eric.wang2@bcm.edu',
-    packages=find_packages(exclude=[])
+    py_modules=['gpu_deploy'],
+    python_requires='<3'
 )
